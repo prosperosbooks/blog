d05f1326b79f09fc2535889547e097e74ff94618
Emin <allen.emin@gmail.com>
2019-01-26 03:44:39 -0500

    third commit set up async redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,19 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+// action creator inside action creator. always dispatch other creator.
+// Call 'fetchPosts'
+// Get list of posts
+// Find alll unique userId's from list of posts
+// Iterate over unique userId's
+// Call 'fetchUser with each userId
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
 
@@ -10,12 +23,15 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-export const fetchUser = id => dispatch => {
-  _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
